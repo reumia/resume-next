d6b56052c12cd9d15f07f7b7b9e5d7919c507d07
zzoman <zzoman.com@gmail.com>
2023-02-22 01:01:27 +0900

    feat: add zzoman's resume

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,23 +1,27 @@
 import { Profile } from "@/types";
 
 const PROFILE: Profile = {
-  name: "Name of Developer", // string
-  startAt: new Date(1999, 12, 31), // date
+  name: "조현석",
+  startAt: new Date(2013, 2, 1),
   descriptions:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam blandit lacus quam, sed sodales sapien malesuada vitae. In nec pellentesque sapien. Nunc viverra sapien id est placerat, quis ultrices eros vulputate. Mauris a rhoncus purus. Suspendisse quis laoreet massa. Aliquam laoreet maximus odio, et viverra dolor scelerisque vitae. Duis nisl mauris, porta et mollis vel, sollicitudin sed turpis. Sed nec placerat urna.",
-  image: "https://pbs.twimg.com/profile_images/1103991968/____400x400.png", // url
+    "Front-end 주력의 JavaScript 개발자입니다.<br/>Naver 서비스의 Markup을 통해 HTML/SCSS의 차별화된 전문성을 쌓았으며, Vue/React 등의 Modern View Framework의 경험과 Node.js Express를 이용한 API WAS 개발, Client 도구개발 등 JavaScript 생태계를 폭 넓게 경험했습니다.  JAVA, PHP 등의 일반적인 Back-end환경과의 협업과 Heroku, Firebase등을 이용한 REST, Serverless 환경 기반 개발 역량을 갖추었습니다.",
+  image: "/profile-min.png",
   socials: [
     {
-      name: "Email", // string
-      url: "mailto:hong@gildong.com", // url
+      name: "Email",
+      url: "mailto:reumia@gmail.com",
     },
     {
       name: "Blog",
-      url: "https://honggildong.com",
+      url: "https://dev.zzoman.com",
     },
     {
       name: "Github",
-      url: "https://github.com/honggildong",
+      url: "https://github.com/reumia",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/hyunsuk-jo-87128310a/",
     },
   ],
 };
