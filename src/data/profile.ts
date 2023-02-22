07eb1748fe30599c4a0b4e609877b3adc7052653
zzoman <zzoman.com@gmail.com>
2023-02-22 23:14:49 +0900

    feat: update the resume

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -4,7 +4,7 @@ const PROFILE: Profile = {
   name: "조현석",
   startAt: new Date(2013, 2, 1),
   descriptions:
-    "Front-end 주력의 JavaScript 개발자입니다.<br/>Naver 서비스의 Markup을 통해 HTML/SCSS의 차별화된 전문성을 쌓았으며, Vue/React 등의 Modern View Framework의 경험과 Node.js Express를 이용한 API WAS 개발, Client 도구개발 등 JavaScript 생태계를 폭 넓게 경험했습니다.  JAVA, PHP 등의 일반적인 Back-end환경과의 협업과 Heroku, Firebase등을 이용한 REST, Serverless 환경 기반 개발 역량을 갖추었습니다.",
+    "Front-end 주력의 JavaScript 개발자입니다.<br/>Naver 서비스의 Markup을 통해 HTML/SCSS의 차별화된 전문성을 쌓았으며, React/Next.js 등의 Modern View Framework의 경험과 Node.js Express를 이용한 API WAS 개발, Client 도구개발 등 JavaScript 생태계를 폭 넓게 경험했습니다.  JAVA, PHP, Python 등의 일반적인 RESTful, GraphQL 환경의 협업 경험, Firebase, Heroku 등을 이용한 Serverless 환경 기반 개발 역량을 갖추었습니다.",
   image: "/profile-min.png",
   socials: [
     {
